8f05cd12decf40e0acce8df45227b4e71bbdaf32
Dev-Iyke <iyke.thompson01@gmail.com>
2025-06-14 22:46:41 +0100

    fixed cors issue

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ const cors = require("cors")
 const routes = require("./routes")
 const app =  express()
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 dotenv.config()
 const url = process.env.MONGODB_URL
 const port = process.env.PORT
+
+const allowedOrigins = [
+  'https://my-movie-guide.vercel.app',
+  'http://localhost:3000' // optional for local dev
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, Postman etc)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true, // if you're using cookies or auth headers
+}));
 
 //Interact with the database using mongoose(connected by connection string)
 mongoose.connect(url)
